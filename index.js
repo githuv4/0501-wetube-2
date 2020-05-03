ef1477fc1d173bcb3ae05b949d0018a9ec044c85
githuv4 <sendex485@gmail.com>
2020-05-04 07:12:29 +0900

    express, get, req/res

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on : http://Localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send("Hello from Home");
+}
+
+function handleProfile(req, res) {
+  res.send("Profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
