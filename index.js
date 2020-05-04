65a042134f237fe23354d10f17cef09de8594554
githuv4 <sendex485@gmail.com>
2020-05-05 07:35:23 +0900

    Babel and middlewares

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on : http://Localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
-  res.send("Hello from Home");
-}
+const handleHome = (req, res) => res.send("Hello from Home 2");
+const handleProfile = (req, res) => res.send("Profile");
 
-function handleProfile(req, res) {
-  res.send("Profile");
-}
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
