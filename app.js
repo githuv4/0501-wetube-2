3e4f127c87cb5cb9fe1d2b290eea969ba60aa5f7
githuv4 <sendex485@gmail.com>
2020-05-07 08:30:49 +0900

    routers

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import routes from "./routes";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
@@ -16,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("combined"));
 app.use(helmet());
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.vidieos, videoRouter);
 
 export default app;
