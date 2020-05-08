d2d162c47bf2e17c71b6d6a03d9d72d9fb3cc635
githuv4 <sendex485@gmail.com>
2020-05-08 11:06:07 +0900

    add the contents of userController

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 app.use(helmet());
 
 app.use(routes.home, globalRouter);
