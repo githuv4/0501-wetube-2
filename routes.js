4a753470b6bc34bc39352888c83ab9df9cb84c60
githuv4 <sendex485@gmail.com>
2020-05-08 12:32:43 +0900

    Revert "add contents of userRouter, videoRouter, videoController"
    This reverts commit daaa6413b71d97d6d62beb5e3b0c4997b0269663.

--- routes.js
+++ routes.js
@@ -28,7 +28,7 @@ const routes = {
   userDetail: USER_DETAIL,
   editProfile: EDIT_PROFILE,
   changePassword: CHANGE_PASSWORD,
-  videos: VIDEOS,
+  vidieos: VIDEOS,
   upload: UPLOAD,
   videoDetail: VIDEO_DETAIL,
   editVideo: EDIT_VIDEO,
