44a23ff00d91cb477e6a90c2e28ea949910c5d7a
githuv4 <sendex485@gmail.com>
2020-05-08 10:48:27 +0900

    controllers

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import routes from "../routes";
+import { home } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send("Home"));
+globalRouter.get(routes.home, home);
 globalRouter.get(routes.join, (req, res) => res.send("Join"));
 globalRouter.get(routes.login, (req, res) => res.send("Login"));
 globalRouter.get(routes.logout, (req, res) => res.send("Log out"));
