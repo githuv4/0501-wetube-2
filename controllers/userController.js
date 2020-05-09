a24d3d557af15c010c9f32c13c5f5afe8bfe9d19
githuv4 <sendex485@gmail.com>
2020-05-09 15:35:09 +0900

    layouts, partials:include -fontawesome

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,4 +4,4 @@ export const logout = (req, res) => res.send("Logout");
 export const users = (req, res) => res.send("Users");
 export const userDetail = (req, res) => res.send("User Detail");
 export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
+export const changePassword = (req, res) => res.render("changePassword");
