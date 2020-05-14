969024eb6b613ef1869fb13d3bd90f14a3d76cda
githuv4 <sendex485@gmail.com>
2020-05-14 15:42:05 +0900

    Log In

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,12 +6,13 @@ import {
   editProfile,
   changePassword,
 } from "../controllers/userController";
+import { localsMiddlewares } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.home, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
--- routes.js
+++ routes.js
@@ -9,11 +9,12 @@ const SEARCH = "/search";
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
-const CHANGE_PASSWORD = "/change-passowrd";
+const CHANGE_PASSWORD = "/change-password";
 
 // Videos
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
+const UPLOAD_VIDEOS = "/videos/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
@@ -25,12 +26,25 @@ const routes = {
   logout: LOGOUT,
   search: SEARCH,
   users: USERS,
-  userDetail: USER_DETAIL,
+  userDetail: (id) => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
   editProfile: EDIT_PROFILE,
   changePassword: CHANGE_PASSWORD,
   videos: VIDEOS,
   upload: UPLOAD,
-  videoDetail: VIDEO_DETAIL,
+  videoDetail: (id) => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  uploadVideos: UPLOAD_VIDEOS,
   editVideo: EDIT_VIDEO,
   deleteVideo: DELETE_VIDEO,
 };
